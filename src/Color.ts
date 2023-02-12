6cf34d1da0585d3bfab6ac333d20fe79b6be8bab
David Li <david@davidl.me>
2023-02-12 02:31:53 -0500

    Convert Color and ColorGradient to typescript

--- src/Color.ts
+++ src/Color.ts
@@ -1,36 +1,33 @@
-const colorConvert = require("color-convert");
-Math.clamp = require("clamp");
-Math.lerp = require("lerp");
+import clamp from "clamp";
+import lerp from "lerp";
+import colorConvert from "color-convert";
 
 export default class Color {
-  constructor(color) {
-    this.r = 0;
-    this.g = 0;
-    this.b = 0;
-    this.a = 1;
+  r: number = 0;
+  g: number = 0;
+  b: number = 0;
+  a: number = 1;
+  input: any;
+
+  constructor(color: any) {
     if (color == null) {
       color = {};
     }
-    let colorType = "rgb255";
-    if (color.type != null) {
-      colorType = color.type;
-    }
-
     if (color.type == "rgb255") {
-      this.r = Math.clamp(color.r / 255, 0, 1);
-      this.g = Math.clamp(color.g / 255, 0, 1);
-      this.b = Math.clamp(color.b / 255, 0, 1);
+      this.r = clamp(color.r / 255, 0, 1);
+      this.g = clamp(color.g / 255, 0, 1);
+      this.b = clamp(color.b / 255, 0, 1);
       if (color.a != null) {
-        this.a = Math.clamp(color.a / 255, 0, 1);
+        this.a = clamp(color.a / 255, 0, 1);
       } else {
         this.a = 1;
       }
     } else if (color.type == "rgb01") {
-      this.r = Math.clamp(color.r, 0, 1);
-      this.g = Math.clamp(color.g, 0, 1);
-      this.b = Math.clamp(color.b, 0, 1);
+      this.r = clamp(color.r, 0, 1);
+      this.g = clamp(color.g, 0, 1);
+      this.b = clamp(color.b, 0, 1);
       if (color.a != null) {
-        this.a = Math.clamp(color.a, 0, 1);
+        this.a = clamp(color.a, 0, 1);
       } else {
         this.a = 1;
       }
@@ -60,12 +57,11 @@ export default class Color {
     Object.freeze(this);
   }
 
-  getRGB255() {
+  getRGB255(): [number, number, number] {
     return [
-      Math.clamp(Math.floor(this.r * 256), 0, 255),
-      Math.clamp(Math.floor(this.g * 256), 0, 255),
-      Math.clamp(Math.floor(this.b * 256), 0, 255),
-      Math.clamp(Math.floor(this.a * 256), 0, 255),
+      clamp(Math.floor(this.r * 256), 0, 255),
+      clamp(Math.floor(this.g * 256), 0, 255),
+      clamp(Math.floor(this.b * 256), 0, 255),
     ];
   }
 
@@ -76,26 +72,26 @@ export default class Color {
   }
 
   getHex() {
-    return colorConvert.rgb.hex(...this.getRGB255());
+    return colorConvert.rgb.hex(this.getRGB255());
   }
 
   getHSV() {
-    return colorConvert.rgb.hsv(...this.getRGB255());
+    return colorConvert.rgb.hsv(this.getRGB255());
   }
 
   getHSL() {
-    return colorConvert.rgb.hsl(...this.getRGB255());
+    return colorConvert.rgb.hsl(this.getRGB255());
   }
 
   getLCH() {
-    return colorConvert.rgb.lch(...this.getRGB255());
+    return colorConvert.rgb.lch(this.getRGB255());
   }
 
   toCSS() {
     return `rgba(${this.getRGB255().join(", ")})`;
   }
 
-  static fromRGB255Array(arr) {
+  static fromRGB255Array(arr: number[]) {
     return new Color({
       type: "rgb255",
       r: arr[0],
@@ -104,61 +100,60 @@ export default class Color {
     });
   }
 
-  static lerp(color0, color1, t, mode = "rgb") {
-    if (mode === "rgb") {
-      return new Color({
-        type: "rgb01",
-        r: Math.lerp(color0.r, color1.r, t),
-        g: Math.lerp(color0.g, color1.g, t),
-        b: Math.lerp(color0.b, color1.b, t),
-        a: Math.lerp(color0.a, color1.a, t)
-      });
-    } else if (mode === "hsv") {
+  static lerp(color0: Color, color1: Color, t: number, mode: string = "rgb") {
+    if (mode === "hsv") {
       let hsv0 = color0.getHSV();
       let hsv1 = color1.getHSV();
       return new Color({
         type: "hsv",
-        h: Math.lerp(hsv0[0], hsv1[0], t),
-        s: Math.lerp(hsv0[1], hsv1[1], t),
-        v: Math.lerp(hsv0[2], hsv1[2], t)
+        h: lerp(hsv0[0], hsv1[0], t),
+        s: lerp(hsv0[1], hsv1[1], t),
+        v: lerp(hsv0[2], hsv1[2], t)
       });
     } else if (mode === "hsl") {
       const hsl0 = color0.getHSL();
       const hsl1 = color1.getHSL();
       const flipHueDirection = (Math.abs(hsl0[0] - hsl1[0]) > 180);
-      const hsl0Updated = hsl0[0] + 360 * (flipHueDirection && hsl0[0] < hsl1[0]);
-      const hsl1Updated = hsl1[0] + 360 * (flipHueDirection && hsl1[0] < hsl0[0]);
-      const intermediateHue = Math.lerp(
+      const hsl0Updated = hsl0[0] + 360 * Number(flipHueDirection && hsl0[0] < hsl1[0]);
+      const hsl1Updated = hsl1[0] + 360 * Number(flipHueDirection && hsl1[0] < hsl0[0]);
+      const intermediateHue = lerp(
         hsl0Updated, hsl1Updated, t);
       return new Color({
         type: "hsl",
         h: intermediateHue,
-        s: Math.lerp(hsl0[1], hsl1[1], t),
-        l: Math.lerp(hsl0[2], hsl1[2], t)
+        s: lerp(hsl0[1], hsl1[1], t),
+        l: lerp(hsl0[2], hsl1[2], t)
       });
     } else if (mode === "hsl_flip") {
       const hsl0 = color0.getHSL();
       const hsl1 = color1.getHSL();
       const flipHueDirection = (Math.abs(hsl0[0] - hsl1[0]) > 180);
-      const hsl0Updated = hsl0[0] + 360 * (!flipHueDirection && hsl0[0] < hsl1[0]);
-      const hsl1Updated = hsl1[0] + 360 * (!flipHueDirection && hsl1[0] < hsl0[0]);
-      const intermediateHue = Math.lerp(
+      const hsl0Updated = hsl0[0] + 360 * Number(!flipHueDirection && hsl0[0] < hsl1[0]);
+      const hsl1Updated = hsl1[0] + 360 * Number(!flipHueDirection && hsl1[0] < hsl0[0]);
+      const intermediateHue = lerp(
         hsl0Updated, hsl1Updated, t);
       return new Color({
         type: "hsl",
         h: intermediateHue,
-        s: Math.lerp(hsl0[1], hsl1[1], t),
-        l: Math.lerp(hsl0[2], hsl1[2], t)
+        s: lerp(hsl0[1], hsl1[1], t),
+        l: lerp(hsl0[2], hsl1[2], t)
       });
     } else if (mode == "lch") {
       let lch0 = color0.getLCH();
       let lch1 = color1.getLCH();
       return new Color({
         type: "lch",
-        l: Math.lerp(lch0[0], lch1[0], t),
-        c: Math.lerp(lch0[1], lch1[1], t),
-        h: Math.lerp(lch0[2], lch1[2], t)
+        l: lerp(lch0[0], lch1[0], t),
+        c: lerp(lch0[1], lch1[1], t),
+        h: lerp(lch0[2], lch1[2], t)
       });
     }
+    return new Color({
+      type: "rgb01",
+      r: lerp(color0.r, color1.r, t),
+      g: lerp(color0.g, color1.g, t),
+      b: lerp(color0.b, color1.b, t),
+      a: lerp(color0.a, color1.a, t)
+    });
   }
 }

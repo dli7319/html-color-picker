f5ca0ee4ef5039e68d58c8deec6205dbd221d814
David Li <david@davidl.me>
2025-02-01 04:39:57 +0000

    Merge globals and declarations

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,5 +1,18 @@
 declare module "*.module.css";
+declare module "*.css";
 
 declare module "lerp" {
   export default function lerp(a: number, b: number, t: number): number;
+}
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    "other-tools": any;
+    "color-interpolation": any;
+  }
+}
+
+declare module "*?raw" {
+  const content: string;
+  export default content;
 }

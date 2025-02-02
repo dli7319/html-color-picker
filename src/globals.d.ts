64c0df2ab019a77e5bdb0e8a492db548d6939101
David Li <david@davidl.me>
2025-02-02 06:31:49 +0000

    Clean up globals.d.ts

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,14 +1,6 @@
 declare module "*.module.css";
 declare module "*.css";
 
-declare namespace JSX {
-  interface IntrinsicElements {
-    "other-tools": any;
-    "color-interpolation": any;
-    "sl-copy-button": any;
-  }
-}
-
 declare module "*?raw" {
   const content: string;
   export default content;

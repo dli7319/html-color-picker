f59d3be27dd697f266b5f4ca2af34191cb1887e0
David Li <david@davidl.me>
2025-02-02 07:02:46 +0000

    Format ColorPickerSetInterpolationActiveEvent.ts

--- src/ColorPickerSetInterpolationActiveEvent.ts
+++ src/ColorPickerSetInterpolationActiveEvent.ts
@@ -1,11 +1,14 @@
 import { ActiveColorSide } from "./ColorInterpolation";
 
 export class ColorPickerSetInterpolationActiveEvent extends Event {
-    static readonly eventName = "set-interpolation-active";
-    active: ActiveColorSide;
-    
-    constructor(active: ActiveColorSide) {
-        super(ColorPickerSetInterpolationActiveEvent.eventName, { bubbles: true, composed: true });
-        this.active = active;
-    }
+  static readonly eventName = "set-interpolation-active";
+  active: ActiveColorSide;
+
+  constructor(active: ActiveColorSide) {
+    super(ColorPickerSetInterpolationActiveEvent.eventName, {
+      bubbles: true,
+      composed: true,
+    });
+    this.active = active;
+  }
 }
